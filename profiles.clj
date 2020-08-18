2492eaa6b0cc1801acfeaa65c8e323dd1f0e56f0
Abel Quiros <beluchin@gmail.com>
2020-08-18 07:54:36 -0400

    initial

---
+++ profiles.clj
@@ -0,0 +1,11 @@
+
+{:user {:plugins [
+                  ;; middleware for refactoring
+                  ;; https://github.com/clojure-emacs/refactor-nrepl
+                  ;[refactor-nrepl "2.5.0"]
+                  ;[cider/cider-nrepl "0.25.1"]
+
+                  [lein-try "0.4.3"]
+                  [jonase/eastwood "0.3.3"]
+                  [lein-kibit "0.1.6"]]}}
+
